6751ac8a6dba330222a2771351a314b2648554a0
ikesato <ikesato76@gmail.com>
2015-01-29 11:16:39 +0900

    Cosmetic change for ordering year

--- spec/ehou/ehou_string_spec.rb
+++ spec/ehou/ehou_string_spec.rb
@@ -8,15 +8,15 @@ describe Ehou::EhouString do
     [e.ehou.angle, e.ehou]
   end
   it 'should select correct ehous' do
-    expect(get_ehou(2014)).to eq [ 75, "東北東"]
-    expect(get_ehou(2019)).to eq [ 75, "東北東"]
     expect(get_ehou(2010)).to eq [255, "西南西"]
-    expect(get_ehou(2015)).to eq [255, "西南西"]
     expect(get_ehou(2011)).to eq [165, "南南東"]
-    expect(get_ehou(2013)).to eq [165, "南南東"]
-    expect(get_ehou(2016)).to eq [165, "南南東"]
-    expect(get_ehou(2018)).to eq [165, "南南東"]
     expect(get_ehou(2012)).to eq [345, "北北西"]
+    expect(get_ehou(2013)).to eq [165, "南南東"]
+    expect(get_ehou(2014)).to eq [ 75, "東北東"]
+    expect(get_ehou(2015)).to eq [255, "西南西"]
+    expect(get_ehou(2016)).to eq [165, "南南東"]
     expect(get_ehou(2017)).to eq [345, "北北西"]
+    expect(get_ehou(2018)).to eq [165, "南南東"]
+    expect(get_ehou(2019)).to eq [ 75, "東北東"]
   end
 end
