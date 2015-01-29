9fb5241d2ce0b54ae62e1217b76234113826582c
ikesato <ikesato76@gmail.com>
2015-01-29 21:50:16 +0900

    Add specs when calling twice

--- spec/ehou/ehou_extend_spec.rb
+++ spec/ehou/ehou_extend_spec.rb
@@ -13,17 +13,29 @@ describe Ehou::EhouExtend do
       expect(Date.new(2014).ehou.angle).to eq 75
       expect(Date.new(2015).ehou.angle).to eq 255
     end
+
+    it 'should return same object when calling twice' do
+      d = Date.new(2014)
+      last = d.ehou
+      expect(d.ehou.object_id).to eq last.object_id
+    end
   end
 
   describe Time do
     it 'should return ehou string' do
-      expect(Time.new(2014).ehou).to eq "東北東"
-      expect(Time.new(2015).ehou).to eq "西南西"
+      expect(Time.local(2014).ehou).to eq "東北東"
+      expect(Time.local(2015).ehou).to eq "西南西"
     end
 
     it 'should return ehou angle' do
-      expect(Time.new(2014).ehou.angle).to eq 75
-      expect(Time.new(2015).ehou.angle).to eq 255
+      expect(Time.local(2014).ehou.angle).to eq 75
+      expect(Time.local(2015).ehou.angle).to eq 255
+    end
+
+    it 'should return same object when calling twice' do
+      d = Time.local(2014)
+      last = d.ehou
+      expect(d.ehou.object_id).to eq last.object_id
     end
   end
 
