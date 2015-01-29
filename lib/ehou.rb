53158b6325b131158293cf14a5358b36d5623f85
ikesato <ikesato76@gmail.com>
2015-01-29 11:02:30 +0900

    Cosmetic change using single quote for require

--- lib/ehou.rb
+++ lib/ehou.rb
@@ -1,7 +1,7 @@
-require "ehou/version"
-require "ehou/ehou_date"
-require "ehou/ehou_time"
-require "ehou/ehou_string"
+require 'ehou/version'
+require 'ehou/ehou_date'
+require 'ehou/ehou_time'
+require 'ehou/ehou_string'
 
 module Ehou
 end
